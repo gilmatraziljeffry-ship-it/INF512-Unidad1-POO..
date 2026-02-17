99c9e842f178e0a872ab35aa498580764d81585e
gilmatraziljeffry-ship-it <gilmatraziljeffry@gmail.com>
2026-02-17 16:49:25 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ProductoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto miProducto = new Producto(1, "Laptop", 850.00, 10);
+            miProducto.MostrarProducto();
+
+            Console.WriteLine("\n--- Probando asignación de valores inválidos ---");
+
+            miProducto.Precio = -50.0;
+            miProducto.Stock = -5;
+
+            Console.WriteLine("\n--- Probando método DescontarStock ---");
+
+            miProducto.DescontarStock(4);
+            miProducto.DescontarStock(10);
+
+            Console.WriteLine("\n--- Estado final del producto ---");
+            miProducto.MostrarProducto();
+
+            Console.ReadKey();
+        }
+    }
+}
